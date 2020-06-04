c2fe03c5e00340493651d2eaabc29d7e6540efbe
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-05 00:44:45 +0200

    Create custom response to handle 401 HTTP status

---
+++ src/main/java/pl/edu/pb/mongodbapplication/config/error/CustomExceptionHandler.java
@@ -0,0 +1,31 @@
+package pl.edu.pb.mongodbapplication.config.error;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pl.edu.pb.mongodbapplication.config.error.response.AuthenticationResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"unchecked","rawtypes"})
+@ControllerAdvice
+@PropertySource("classpath:messages.properties")
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Value("${loginFailed}")
+    private String loginFailed;
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<Object> handleRecordNotFoundException(AuthenticationException exception) {
+        List<String> details = new ArrayList<>();
+        details.add(exception.getLocalizedMessage());
+        AuthenticationResponse error = new AuthenticationResponse(loginFailed, details, HttpStatus.UNAUTHORIZED.value());
+        return new ResponseEntity(error, HttpStatus.UNAUTHORIZED);
+    }
+}
