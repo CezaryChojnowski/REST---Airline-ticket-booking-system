31944998713823bdefa5879dd238c39de33ab946
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-05 00:17:50 +0200

    Create LoginRequest and SignupRequest models

---
+++ src/main/java/pl/edu/pb/mongodbapplication/payload/request/SignupRequest.java
@@ -0,0 +1,48 @@
+package pl.edu.pb.mongodbapplication.payload.request;
+
+import java.util.Set;
+
+public class SignupRequest {
+
+    private String username;
+
+
+    private String email;
+
+    private Set<String> roles;
+
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return this.roles;
+    }
+
+    public void setRole(Set<String> roles) {
+        this.roles = roles;
+    }
+}
