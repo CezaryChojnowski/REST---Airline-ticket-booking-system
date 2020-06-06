2e455a9f49442d62743b4f8152cb2b4e51970853
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-06 13:14:02 +0200

    Fix message properties

--- src/main/java/pl/edu/pb/mongodbapplication/config/AppConfig.java
+++ src/main/java/pl/edu/pb/mongodbapplication/config/AppConfig.java
@@ -3,32 +3,20 @@ package pl.edu.pb.mongodbapplication.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public LocalValidatorFactoryBean localValidatorFactoryBean() {
-        return new LocalValidatorFactoryBean();
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+        validatorFactoryBean.setValidationMessageSource(messageSource);
+        return validatorFactoryBean;
     }
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener(LocalValidatorFactoryBean lfb) {
         return new ValidatingMongoEventListener(lfb);
     }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource
-                = new ReloadableResourceBundleMessageSource();
-
-        messageSource.setBasename("classpath:messages.properties");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-
-
-
 }
