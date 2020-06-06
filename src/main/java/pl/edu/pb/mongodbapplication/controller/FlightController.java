19e5500da9d8944be1f3e7a0a452eca0d5bd6765
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-07 00:01:41 +0200

    Create POST method to create flight by admin account

--- src/main/java/pl/edu/pb/mongodbapplication/controller/FlightController.java
+++ src/main/java/pl/edu/pb/mongodbapplication/controller/FlightController.java
@@ -1,15 +1,16 @@
 package pl.edu.pb.mongodbapplication.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pb.mongodbapplication.model.Flight;
+import pl.edu.pb.mongodbapplication.payload.response.MessageResponse;
 import pl.edu.pb.mongodbapplication.service.CountryService;
 import pl.edu.pb.mongodbapplication.service.FlightService;
+import pl.edu.pb.mongodbapplication.service.TicketService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,17 +19,27 @@ public class FlightController {
 
     private final FlightService flightService;
 
+    private final TicketService ticketService;
+
     private final CountryService countryService;
 
     @Autowired
-    public FlightController(FlightService flightService, CountryService countryService) {
+    public FlightController(FlightService flightService, TicketService ticketService, CountryService countryService) {
         this.flightService = flightService;
+        this.ticketService = ticketService;
         this.countryService = countryService;
     }
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(method = RequestMethod.GET)
     public List<Flight> findAllFlights(){
         return flightService.findAllFlights();
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity createFlight(@Valid @RequestBody Flight flight){
+        flightService.createFlight(flight);
+        return ResponseEntity.ok(new MessageResponse("Flight creating successfully!"));
     }
 
     @PreAuthorize("hasRole('USER')")
