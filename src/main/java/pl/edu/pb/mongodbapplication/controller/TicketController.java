16de595fa4eadee588af1b3439b31ee623e0f293
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-07 00:02:08 +0200

    Create get method to get all tickets by username

--- src/main/java/pl/edu/pb/mongodbapplication/controller/TicketController.java
+++ src/main/java/pl/edu/pb/mongodbapplication/controller/TicketController.java
@@ -1,17 +1,20 @@
 package pl.edu.pb.mongodbapplication.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pb.mongodbapplication.DTO.TicketDTO;
+import pl.edu.pb.mongodbapplication.DTO.TicketDTOForTicketsListByUser;
 import pl.edu.pb.mongodbapplication.model.Flight;
-import pl.edu.pb.mongodbapplication.payload.response.MessageResponse;
+import pl.edu.pb.mongodbapplication.model.Ticket;
 import pl.edu.pb.mongodbapplication.service.TicketService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
+@Validated
 public class TicketController {
     private final TicketService ticketService;
 
@@ -21,10 +24,16 @@ public class TicketController {
 
     @PreAuthorize("hasRole('USER')")
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity bookingFlight(@RequestBody Flight flight){
-            ticketService.bookingFlight(flight);
-        return ResponseEntity.ok(new MessageResponse("Flight booking completed successfully!"));
+    public TicketDTO bookingFlight(@Valid @RequestBody Flight flight){
+        Ticket ticket = ticketService.bookingFlight(flight);
+        return ticketService.getCreatedTicketDTO(ticket);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(method = RequestMethod.GET)
+    public List<TicketDTOForTicketsListByUser> getAllTicketsByUserName(
+            @RequestParam String username){
+        return ticketService.findAllTicketsByUser(username);
+    }
 
 }
