ad1fcdf69a71c5422c9e474df2fb7723240eca5e
Cezary Chojnowski <cezary.mateusz.chojnowski@gmail.com>
2020-06-06 23:55:33 +0200

    Create method to createFlight from admin account Create method to valid airPort model in Flight

--- src/main/java/pl/edu/pb/mongodbapplication/service/FlightService.java
+++ src/main/java/pl/edu/pb/mongodbapplication/service/FlightService.java
@@ -2,10 +2,15 @@ package pl.edu.pb.mongodbapplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edu.pb.mongodbapplication.config.error.InvalidDataException;
 import pl.edu.pb.mongodbapplication.config.error.NoFlightsBetweenTheseCitiesOnThisDayException;
+import pl.edu.pb.mongodbapplication.model.AirPort;
 import pl.edu.pb.mongodbapplication.model.Flight;
 import pl.edu.pb.mongodbapplication.repository.FlightRepository;
 
+import javax.validation.ConstraintViolationException;
+import java.lang.reflect.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -39,6 +44,33 @@ public class FlightService {
                     " on " +
                     date);
         }
+    }
+
+    public Flight createFlight(Flight flight){
+        List<Boolean> booleanList = new ArrayList<>();
+        AirPort airPortFrom = flight.getAirPortFrom();
+        AirPort airPortTo = flight.getAirPortTo();
+        booleanList.add(airPortIsValid(airPortFrom));
+        booleanList.add(airPortIsValid(airPortTo));
+        Boolean resultValid = booleanList.contains(Boolean.TRUE);
+        if(resultValid){
+            throw new InvalidDataException("All fields are required");
+        }
+        return flightRepository.save(flight);
+    }
+
+    public boolean airPortIsValid(AirPort airport){
+        List<Boolean> booleanList = new ArrayList<>();
+        if(airport.getCountry()==null || airport.getCountry().isEmpty()){
+            booleanList.add(false);
+        }
+        if(airport.getCity()==null || airport.getCity().isEmpty()){
+            booleanList.add(false);
+        }
+        if(airport.getAirPortName()==null || airport.getAirPortName().isEmpty()){
+            booleanList.add(false);
+        }
+        return booleanList.contains(Boolean.FALSE);
 
     }
 
